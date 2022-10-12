ecbdcb95577834a08cf92eb5bc403ea9a932920e
sh1nez <biv888iii888@yandex.ru>
2022-10-13 01:43:54 +0300

    rewrite main filter

--- transcript.py
+++ transcript.py
@@ -1,7 +1,9 @@
 import subprocess
 
+
 def to_text(image_path):
     command = "tesseract '" + image_path + "' stdout"
     print(command)
-    result = subprocess.run(command, capture_output=True, text=True, shell=True)
+    result = subprocess.run(
+        command, capture_output=True, text=True, shell=True)
     return result.stdout.split("\n")
