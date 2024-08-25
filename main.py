bf1c64477b4610b50e9b4f1dded23e579568dd81
sh1nez <biv888iii888@yandex.ru>
2024-08-26 01:21:40 +0300

    groups and non standart promo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from pyrogram import Client, filters, types
 from transcript import to_text
 from stealth import promo
-from pyrogram import Client, filters, types
 from datetime import datetime
 import random
+import re
 
 name = "freebet"
 
@@ -12,39 +13,59 @@ channels = ["-1001755139624", "-1001889498592",
             "-1001810104257", "-1001525747974", "-1001979310355"]
 
 
+name_cnt = 0
+
+
 @app.on_message()
 async def filter_messages(cli, message: types.Message):
     if str(message.chat.id) not in channels:
         return
-    print(datetime.now().strftime('%H:%M:%S'), "triggered")
     words = []
     if message.__dict__["media"]:
+        global name_cnt
+        name_cnt += 1
         try:
-            name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ".jpg"
+            name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + \
+                str(name_cnt) + ".jpg"
+            if name_cnt > 9:
+                name_cnt = 0
             await message.download(name)
             words = to_text("downloads/" + name)
         except Exception:
+            # continue
             pass
 
+    string = None
     if message.__dict__['text']:
         words.extend(message.text.split())
+        string = message.__dict__['text']
 
     if message.__dict__['caption']:
         words.extend(message.caption.split())
+        string = message.__dict__['caption']
 
     new = []
-    print("check promo")
     for i in words:
         if "PP" in i:
             inx = i.find("PP")
             new.append(i[inx:10])
+
     words = new
+
     if not words:
-        return
+        if not string:
+            return
+        url_pattern = re.compile(r"https?://\S+")
+        string = url_pattern.sub("", string)
+        words = re.findall(r'\b[a-zA-Z0-9]{10,}\b', string)
+        if not words:
+            return
     else:
+        print(datetime.now().strftime('%H:%M:%S'), "triggered")
         print("using promo...")
     random.shuffle(words)
     for i in words:
         promo(i)
 
+print("start...")
 app.run()
