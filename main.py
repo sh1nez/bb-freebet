72c7b87e6e7be8f6b77c9a1ebdc4eb1fc6a7fd69
sh1nez <biv888iii888@yandex.ru>
2024-09-04 01:39:55 +0300

    v1 server-client

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from pyrogram import Client, types
 from transcript import to_text
-from stealth import promo
 from datetime import datetime
+import socket
+import sys
 import random
 import re
 import logging
 
-logger = logging.getLogger('my_logger')
+logger = logging.getLogger('freebet')
 
 logging.basicConfig(
     # (DEBUG, INFO, WARNING, ERROR, CRITICAL)
     level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('log.txt'),
-        # logging.StreamHandler()  # Вывод в консоль
+        logging.StreamHandler()  # Вывод в консоль
+        # logging.FileHandler('log.txt'),
     ]
 )
 
@@ -25,8 +26,47 @@ app = Client(name)
 channels = ["-1001755139624", "-1001889498592",
             "-1001810104257", "-1001525747974", "-1001979310355"]
 
-
 name_cnt = 0
+
+clients = []
+
+for i in sys.argv[1:]:
+    clients.append(("localhost", int(i)))
+
+clients_len = len(clients)
+assert clients_len != 0
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def many_clents(promo):
+    for i in clients:
+        send(promo, i)
+
+
+def send(promo, client):
+    logger.warning(f"send promi {promo} to {client[1]}")
+    promo += "<END>"
+    sock.sendto(promo.encode("utf-8"), client)
+
+
+def many_promos(arr: [str]):
+    random.shuffle(clients)
+    for i, promo in enumerate(arr):
+        send(promo, clients[i % clients_len])
+
+
+async def media_to_text(message: types.Message):
+    global name_cnt
+    name_cnt += 1
+    try:
+        name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + str(name_cnt) + ".jpg"
+        if name_cnt > 9:
+            name_cnt = 0
+        await message.download(name)
+        return to_text("downloads/" + name)
+    except Exception:
+        pass
 
 
 @app.on_message()
@@ -35,18 +75,7 @@ async def filter_messages(cli, message: types.Message):
         return
     words = []
     if message.__dict__["media"]:
-        global name_cnt
-        name_cnt += 1
-        try:
-            name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + \
-                str(name_cnt) + ".jpg"
-            if name_cnt > 9:
-                name_cnt = 0
-            await message.download(name)
-            words = to_text("downloads/" + name)
-        except Exception:
-            # continue
-            pass
+        words = await media_to_text(message)
 
     string = None
     if message.__dict__['text']:
@@ -62,7 +91,6 @@ async def filter_messages(cli, message: types.Message):
         if "PP" in i:
             inx = i.find("PP")
             new.append(i[inx:10])
-
     words = new
 
     if not words:
@@ -71,16 +99,14 @@ async def filter_messages(cli, message: types.Message):
         url_pattern = re.compile(r"https?://\S+")
         string = url_pattern.sub("", string)
         words = re.findall(r'\b[a-zA-Z0-9]{10,}\b', string)
-        if not words:
-            return
+        if words:
+            logger.warning("using promo...")
+            for i in words:
+                many_clents(i)
+        return
     else:
-        print(datetime.now().strftime('%H:%M:%S'), "triggered")
-        print("using promo...")
-    random.shuffle(words)
-    for i in words:
-        promo(i)
-
+        logger.warning("using promo...")
+        many_promos(words)
 
 logger.warning("start!")
-
 app.run()
